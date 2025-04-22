57b99e3eb5aaf449efe63c9da048c1317683c66a
GinaShome <ginashome8@gmail.com>
2025-04-21 22:35:33 -0400

    Added dashboard and show .html files, as well as updated urls and views to reflect

--- home/views.py
+++ home/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Plan, Joined, Comment
 from django.contrib.auth.decorators import login_required
-
+'''i made a new version of this, i think this can be deleted
 def index(request):
     template_data = {}
     template_data['title'] = 'Movies Store'
     return render(request, 'home/index.html', {'template_data': template_data})
+'''
 
 def about(request):
     template_data = {}
@@ -40,4 +41,30 @@ def show(request, id):
     template_data['plan'] = plan
     template_data['comments'] = comments
     # TODO need to implement html stuff for below to work! vvv
-    return render(request, 'plans/show.html', {'template_data': template_data})
+    return render(request, 'home/show.html', {'template_data': template_data})
+
+@login_required
+def join_plan(request, id):
+    plan = get_object_or_404(Plan, id=id)
+    if not Joined.objects.filter(plan=plan, user=request.user).exists():
+        Joined.objects.create(plan=plan, user=request.user)
+    return redirect('plan.show', id=id)
+
+@login_required
+def add_comment(request, id):
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        plan = get_object_or_404(Plan, id=id)
+        Comment.objects.create(user=request.user, plan=plan, text=text)
+    return redirect('plan.show', id=id)
+
+@login_required
+def dashboard(request):
+    user = request.user
+    created_plans = Plan.objects.filter(user=user)
+    joined_plans = Plan.objects.filter(joined__user=user)
+
+    return render(request, 'home/dashboard.html', {
+        'created_plans': created_plans,
+        'joined_plans': joined_plans,
+    })
