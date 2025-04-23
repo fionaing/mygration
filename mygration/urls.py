8fe62e6ff353612a4f92172ae9b481030cf99538
”Charlie <charlieciardullo@icloud.com>
2025-04-23 15:07:47 -0400

    fixed serving for user uploaded images. Updated the edit plan page to display the current uploaded image

--- mygration/urls.py
+++ mygration/urls.py
@@ -14,15 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from home import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('', include('home.urls')),
     path('', views.index, name='plans.index'),
-    path('<int:id>/', views.show, name='plans.show'),
+    path('<int:id>/', views.show, name='plans.show')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
